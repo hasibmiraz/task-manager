bf8879d3893fb4869dcf948c0f265cd9bafbc0c8
hasibmiraz <hasibulalam066@gmail.com>
2021-07-11 00:16:25 +0600

    Validation & Sanitization data

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
@@ -9,37 +10,74 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 const User = mongoose.model('User', {
   name: {
     type: String,
+    required: true,
+    trim: true,
   },
+
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Invalid Email ID!');
+      }
+    },
+  },
+
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error(`Password can't containg --Password-- word!`);
+      }
+    },
+  },
+
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number.');
+      }
+    },
   },
 });
 
-const me = new User({
-  name: 'Miraz',
-  age: 'mike',
-});
+// const me = new User({
+//   name: '  Mike',
+//   email: '  MIKE@email.com  ',
+//   password: 'Miraz12345',
+// });
 
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// me.save()
+//   .then(() => {
+//     console.log(me);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 const Tasks = mongoose.model('Tasks', {
   description: {
     type: String,
+    trim: true,
+    required: true,
   },
 
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
 const task = new Tasks({
-  description: 'Clean the house',
+  description: 'Working too',
   completed: true,
 });
 
