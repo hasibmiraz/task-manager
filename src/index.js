4d8665d084ab0ebf970c2727bf0f97b44baaf23b
hasibmiraz <hasibulalam066@gmail.com>
2021-07-12 00:22:13 +0600

    getting tasks

--- src/index.js
+++ src/index.js
@@ -61,6 +61,34 @@ app.post('/tasks', (req, res) => {
     });
 });
 
+// Fetch all tasks
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+// Fetch task by ID
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
