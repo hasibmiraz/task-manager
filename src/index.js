ba0d56339cc31637ecfb10eb52dc1c9038a16280
hasibmiraz <hasibulalam066@gmail.com>
2021-07-11 21:25:53 +0600

    Fetching data from database

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 
+// Create New User
 app.post('/users', (req, res) => {
   const user = new User(req.body);
   user
@@ -19,6 +20,35 @@ app.post('/users', (req, res) => {
     });
 });
 
+// Get All users
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+//Get users by id
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+
+      res.send(user);
+    })
+    .catch((e) => {
+      res.status(500).send();
+    });
+});
+
+// Create New Task
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task
