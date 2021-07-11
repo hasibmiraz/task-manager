93344b6c69e663b9c5dce9ebb84ddbece6f31f42
hasibmiraz <hasibulalam066@gmail.com>
2021-07-11 17:24:59 +0600

    Init task-app

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,91 +1,9 @@
+// E:/web-dev-projects/Database/mongodb/bin/mongod --dbpath=E:/web-dev-projects/Database/mongodb-data
+
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
-
-const User = mongoose.model('User', {
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error('Invalid Email ID!');
-      }
-    },
-  },
-
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minlength: 7,
-    validate(value) {
-      if (value.toLowerCase().includes('password')) {
-        throw new Error(`Password can't containg --Password-- word!`);
-      }
-    },
-  },
-
-  age: {
-    type: Number,
-    default: 0,
-    validate(value) {
-      if (value < 0) {
-        throw new Error('Age must be a positive number.');
-      }
-    },
-  },
-});
-
-// const me = new User({
-//   name: '  Mike',
-//   email: '  MIKE@email.com  ',
-//   password: 'Miraz12345',
-// });
-
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-const Tasks = mongoose.model('Tasks', {
-  description: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-const task = new Tasks({
-  description: 'Working too',
-  completed: true,
-});
-
-task
-  .save()
-  .then(() => {
-    console.log(task);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
---
+++ src/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Invalid Email ID!');
+      }
+    },
+  },
+
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error(`Password can't containg --Password-- word!`);
+      }
+    },
+  },
+
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number.');
+      }
+    },
+  },
+});
+
+module.exports = User;
