8b9622eab893b7aeccfb8fa40165f5b755c76f79
hasibmiraz <hasibulalam066@gmail.com>
2021-07-12 17:44:31 +0600

    Updating single collection

--- src/index.js
+++ src/index.js
@@ -45,6 +45,33 @@ app.get('/users/:id', async (req, res) => {
   }
 });
 
+// Update user by id
+app.patch('/users/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ['name', 'email', 'password', 'age'];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' });
+  }
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
 // Create New Task
 app.post('/tasks', async (req, res) => {
   const task = new Task(req.body);
@@ -78,6 +105,34 @@ app.get('/tasks/:id', async (req, res) => {
     }
 
     res.status(200).send(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Update Tasks
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body);
+  const updateFields = ['description', 'completed'];
+  const validOperation = updates.every((update) =>
+    updateFields.includes(update)
+  );
+
+  if (!validOperation) {
+    return res.status(400).send({ error: 'Invalid request!' });
+  }
+
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).send();
+    }
+
+    res.send(task);
   } catch (e) {
     res.status(500).send(e);
   }
