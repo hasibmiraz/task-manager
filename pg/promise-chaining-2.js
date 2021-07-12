5fd2ef6404dac10ffea053f03a77aea06d616811
hasibmiraz <hasibulalam066@gmail.com>
2021-07-12 16:44:06 +0600

    Switching to async/await

--- pg/promise-chaining-2.js
+++ pg/promise-chaining-2.js
@@ -1,13 +1,26 @@
 require('../src/db/mongoose');
 const Task = require('../src/models/task');
 
-Task.findByIdAndRemove('60eae655ad19c50fb8ff5245')
-  .then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then((result) => {
-    console.log(result);
+// Task.findByIdAndRemove('60eae655ad19c50fb8ff5245')
+//   .then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const deleteTaskAndCount = async (id) => {
+  await Task.findByIdAndDelete(id);
+  return await Task.countDocuments({ completed: false });
+};
+
+deleteTaskAndCount('60eae6a01cee5b15e4d6cc11')
+  .then((count) => {
+    console.log(count);
   })
   .catch((e) => {
     console.log(e);
