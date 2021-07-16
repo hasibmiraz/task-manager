917fffc3f6635f81e41311e171fd07a91bfc46e4
hasibmiraz <hasibulalam066@gmail.com>
2021-07-16 20:56:58 +0600

    Requests wrap up

--- src/index.js
+++ src/index.js
@@ -24,17 +24,3 @@ app.use(userRouter, taskRouter);
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
-
-const jwt = require('jsonwebtoken');
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', {
-    expiresIn: '7 days',
-  });
-  console.log(token);
-
-  const data = jwt.verify(token, 'thisismynewcourse');
-  console.log(data);
-};
-
-myFunction();
